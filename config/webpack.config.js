05f650471861a8c1545030ee6bf71f4816f19cce
yearsyan <yearsyan@hotmail.com>
2023-04-10 23:39:24 +0800

    feat(web): es model

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -8,7 +8,15 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const babelLoaderConfig = {
   loader: 'babel-loader',
   options: {
-    presets: ['@babel/preset-env', '@babel/preset-react']
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+        },
+      ],
+      '@babel/preset-react',
+    ],
   }
 }
 
